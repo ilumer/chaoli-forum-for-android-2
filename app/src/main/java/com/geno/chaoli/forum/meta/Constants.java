8d59d872ce32203a56ddf14d0b77091af4a558cf
daquexian <daquexian566@gmail.com>
2016-06-06 17:57:20 +0800

    xml and images

--- app/src/main/java/com/geno/chaoli/forum/meta/Constants.java
+++ app/src/main/java/com/geno/chaoli/forum/meta/Constants.java
@@ -11,6 +11,8 @@ public class Constants
 	public static final String SIGN_UP_URL = "https://chaoli.club/index.php/user/join?invite=";
 	// activity.json/32/2
 	public static final String GET_ACTIVITIES_URL = "https://chaoli.club/member/activity.json/";
+	// statistics.ajax/32
+	public static final String GET_STATISTICS_URL = "https://chaoli.club/index.php/member/statistics.ajax/";
 	// index.json/channelName?searchDetail
 	// index.json/all?search=%23%E4%B8%8A%E9%99%90%EF%BC%9A0%20~%20100
 	// index.json/chem?search=%23%E7%B2%BE%E5%93%81
@@ -18,7 +20,6 @@ public class Constants
 	// index.json/1430/p2
 	public static final String postListURL = "https://chaoli.club/conversation/index.json/";
 	public static final String loginURL = "https://chaoli.club/index.php/user/login";
-	public static final String postURL = "https://chaoli.club/";// TODO: 16-5-25  to be updated
 	public static final String replyURL = "https://chaoli.club/index.php/?p=conversation/reply.ajax/";
 	public static final String editURL = "https://chaoli.club/index.php/?p=conversation/editPost.ajax/";
 	public static final String cancelEditURL = "https://chaoli.club/index.php/?p=attachment/removeSession/";
