b5a3f41ca4d42a606a80de8999aac5950c4e1743
Y. Z. Chen <754097987@qq.com>
2016-06-11 04:31:25 +0800

    [update] Add support for slash-parenthesis LaTeX, and the vertical alignment.

--- app/src/main/java/com/geno/chaoli/forum/meta/LaTeXtView.java
+++ app/src/main/java/com/geno/chaoli/forum/meta/LaTeXtView.java
@@ -29,7 +29,9 @@ public class LaTeXtView extends TextView
 
 	public static final String SITE = "http://latex.codecogs.com/gif.latex?\\dpi{" + 440 / 2 + "} ";
 
-	public static final Pattern PATTERN = Pattern.compile("(?<=\\$)(.+?)(?=\\$)");
+	public static final Pattern PATTERN1 = Pattern.compile("(?<=\\$)(.+?)(?=\\$)");
+
+	public static final Pattern PATTERN2 = Pattern.compile("(?<=\\\\\\()(.+?)(?=\\\\\\))");
 
 	public static final String TAG = "LaTeXtView";
 
@@ -57,23 +59,38 @@ public class LaTeXtView extends TextView
 		span = new SpannableString(text);
 		((TextView) findViewById(R.id.content)).setText(text);
 		boolean flag = false;
-		Matcher m = PATTERN.matcher(text);
-		while (m.find())
+		Matcher m1 = PATTERN1.matcher(text);
+		while (m1.find())
 		{
 			if (flag = !flag)
 			{
-				final int start = m.start() - 1, end = m.end() + 1;
-				Glide.with(getContext()).load(SITE + m.group()).asBitmap().into(new SimpleTarget<Bitmap>()
+				final int start = m1.start() - 1, end = m1.end() + 1;
+				Glide.with(getContext()).load(SITE + m1.group()).asBitmap().into(new SimpleTarget<Bitmap>()
 				{
 					@Override
 					public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation)
 					{
 						Log.d(TAG, "onResourceReady: executing " + text.substring(start, end));
-						span.setSpan(new ImageSpan(getContext(), resource), start, end, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+						span.setSpan(new ImageSpan(getContext(), resource, DynamicDrawableSpan.ALIGN_BASELINE), start, end, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
 						((TextView) findViewById(R.id.content)).setText(span);
 					}
 				});
 			}
+		}
+		Matcher m2 = PATTERN2.matcher(text);
+		while (m2.find())
+		{
+			final int start = m2.start() - 2, end = m2.end() + 2;
+			Glide.with(getContext()).load(SITE + m2.group()).asBitmap().into(new SimpleTarget<Bitmap>()
+			{
+				@Override
+				public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation)
+				{
+					Log.d(TAG, "onResourceReady: executing " + text.substring(start, end));
+					span.setSpan(new ImageSpan(getContext(), resource, DynamicDrawableSpan.ALIGN_BASELINE), start, end, Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+					((TextView) findViewById(R.id.content)).setText(span);
+				}
+			});
 		}
 	}
 
