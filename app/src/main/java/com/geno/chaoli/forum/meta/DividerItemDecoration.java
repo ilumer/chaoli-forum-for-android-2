8f2d122ccf4d06a8cab823d00fdc0d796bf82832
daquexian <daquexian566@gmail.com>
2016-08-31 22:24:30 +0800

    add icons, delete some deprecated files

---
+++ app/src/main/java/com/geno/chaoli/forum/meta/DividerItemDecoration.java
@@ -0,0 +1,7 @@
+package com.geno.chaoli.forum.meta;
+
+/**
+ * Created by jianhao on 16-8-31.
+ */
+public class DividerItemDecoration {
+}
--- app/src/main/java/com/geno/chaoli/forum/meta/ScrollViewListener.java
+++
@@ -1,8 +0,0 @@
-package com.geno.chaoli.forum.meta;
-
-/**
- * Created by jianhao on 16-5-16.
- */
-public interface ScrollViewListener {
-    void onScrollChanged(FullScreenObservableScrollView scrollView, int x, int y, int oldx, int oldy);
-}
